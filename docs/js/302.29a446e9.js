7335c93a85fc364e16088b00f696de4bb199c6eb
Lo Van Long <info@lovanlong.ga>
2024-01-29 23:58:32 +0700

    build

---
+++ docs/js/302.29a446e9.js
@@ -0,0 +1,257 @@
+"use strict";(globalThis["webpackChunkquasar_project"]=globalThis["webpackChunkquasar_project"]||[]).push([[302],{2302:(t,e,a)=>{a.r(e),a.d(e,{default:()=>D});var o=a(9835),n=a(6970);const d={class:"q-pa-md"},r=["src"],s=(0,o._)("span",{class:"text-caption q-px-sm"},"x",-1),l=["src"];function u(t,e,a,u,i,c){const m=(0,o.up)("q-item-label"),p=(0,o.up)("q-item-section"),g=(0,o.up)("q-badge"),w=(0,o.up)("q-btn"),f=(0,o.up)("q-input"),h=(0,o.up)("q-item"),T=(0,o.up)("q-separator"),_=(0,o.up)("q-list"),y=(0,o.up)("q-icon");return(0,o.wg)(),(0,o.iD)("div",d,[t.cart?.contents?.nodes.length?((0,o.wg)(),(0,o.j4)(_,{key:0,bordered:"",padding:""},{default:(0,o.w5)((()=>[(0,o.Wm)(m,{header:""},{default:(0,o.w5)((()=>[(0,o.Uk)("GIỎ HÀNG")])),_:1}),((0,o.wg)(!0),(0,o.iD)(o.HY,null,(0,o.Ko)(t.cart.contents.nodes,(a=>((0,o.wg)(),(0,o.iD)("div",{key:a.key},[(0,o.Wm)(h,null,{default:(0,o.w5)((()=>[(0,o.Wm)(p,{top:"",thumbnail:"",class:"q-ml-none"},{default:(0,o.w5)((()=>[(0,o._)("img",{src:a.product.node.featuredImage.node.sourceUrl},null,8,r)])),_:2},1024),(0,o.Wm)(p,null,{default:(0,o.w5)((()=>[(0,o.Wm)(m,null,{default:(0,o.w5)((()=>[(0,o.Uk)((0,n.zw)(a.product.node.name)+" ",1),s,(0,o.Wm)(g,null,{default:(0,o.w5)((()=>[(0,o.Uk)((0,n.zw)(a.quantity),1)])),_:2},1024)])),_:2},1024),(0,o.Wm)(m,{caption:""},{default:(0,o.w5)((()=>[(0,o.Uk)("Số tiền: "+(0,n.zw)(a.total),1)])),_:2},1024)])),_:2},1024),(0,o.Wm)(p,{side:"",top:""},{default:(0,o.w5)((()=>[(0,o.Wm)(m,{caption:""},{default:(0,o.w5)((()=>[(0,o.Uk)("Xóa")])),_:1}),(0,o.Wm)(f,{filled:"",value:a.quantity,type:"number",style:{"max-width":"110px"},debounce:"1000","onUpdate:modelValue":e[0]||(e[0]=e=>t.updateCartItem(e)),dense:""},{after:(0,o.w5)((()=>[(0,o.Wm)(w,{onClick:e=>t.removeItemsFromCart(a.key),round:"",dense:"",flat:"",icon:"clear"},null,8,["onClick"])])),_:2},1032,["value"])])),_:2},1024)])),_:2},1024),(0,o.Wm)(T,{spaced:""})])))),128)),(0,o.Wm)(h,null,{default:(0,o.w5)((()=>[(0,o.Wm)(p,null,{default:(0,o.w5)((()=>[(0,o.Uk)("Tạm tính")])),_:1}),(0,o.Wm)(p,{side:"",top:""},{default:(0,o.w5)((()=>[(0,o.Wm)(m,null,{default:(0,o.w5)((()=>[(0,o.Uk)((0,n.zw)(t.cart.subtotal),1)])),_:1})])),_:1})])),_:1}),(0,o.Wm)(h,null,{default:(0,o.w5)((()=>[(0,o.Wm)(p,null,{default:(0,o.w5)((()=>[(0,o.Uk)("Phí vận chuyển")])),_:1}),(0,o.Wm)(p,{side:"",top:""},{default:(0,o.w5)((()=>[(0,o.Wm)(m,null,{default:(0,o.w5)((()=>[(0,o.Uk)((0,n.zw)(t.cart.shippingTotal),1)])),_:1})])),_:1})])),_:1}),(0,o.Wm)(h,null,{default:(0,o.w5)((()=>[(0,o.Wm)(p,null,{default:(0,o.w5)((()=>[(0,o.Wm)(m,null,{default:(0,o.w5)((()=>[(0,o.Uk)("Giảm giá")])),_:1}),(0,o.Wm)(m,{caption:""},{default:(0,o.w5)((()=>[(0,o.Uk)("Áp dụng mã giảm giá")])),_:1})])),_:1}),(0,o.Wm)(p,{side:"",top:""},{default:(0,o.w5)((()=>[(0,o.Wm)(m,null,{default:(0,o.w5)((()=>[(0,o.Uk)((0,n.zw)(t.cart.discountTotal),1)])),_:1})])),_:1})])),_:1}),(0,o.Wm)(T,{spaced:"",inset:"item"}),(0,o.Wm)(h,null,{default:(0,o.w5)((()=>[(0,o.Wm)(p,null,{default:(0,o.w5)((()=>[(0,o.Uk)("Tổng")])),_:1}),(0,o.Wm)(p,{side:"",top:""},{default:(0,o.w5)((()=>[(0,o.Wm)(m,{class:"text-bold"},{default:(0,o.w5)((()=>[(0,o.Uk)((0,n.zw)(t.cart.total),1)])),_:1})])),_:1})])),_:1}),(0,o.Wm)(h,null,{default:(0,o.w5)((()=>[(0,o.Wm)(p,null,{default:(0,o.w5)((()=>[(0,o.Wm)(w,{outline:"",label:"THANH TOÁN",type:"submit",color:"primary"})])),_:1})])),_:1})])),_:1})):(0,o.kq)("",!0),(0,o.Wm)(_,{bordered:"",padding:""},{default:(0,o.w5)((()=>[(0,o.Wm)(m,{header:""},{default:(0,o.w5)((()=>[(0,o.Uk)("SẢN PHẨM")])),_:1}),((0,o.wg)(!0),(0,o.iD)(o.HY,null,(0,o.Ko)(t.products,(e=>((0,o.wg)(),(0,o.iD)("div",{key:e.productId},[(0,o.Wm)(h,null,{default:(0,o.w5)((()=>[(0,o.Wm)(p,{top:"",thumbnail:"",class:"q-ml-none"},{default:(0,o.w5)((()=>[(0,o._)("img",{src:e?.featuredImage.node.sourceUrl},null,8,l)])),_:2},1024),(0,o.Wm)(p,null,{default:(0,o.w5)((()=>[(0,o.Wm)(m,null,{default:(0,o.w5)((()=>[(0,o.Uk)((0,n.zw)(e.name),1)])),_:2},1024),(0,o.Wm)(m,{caption:""},{default:(0,o.w5)((()=>[(0,o.Uk)("Giá: "+(0,n.zw)(e.price),1)])),_:2},1024)])),_:2},1024),(0,o.Wm)(p,{side:"",top:""},{default:(0,o.w5)((()=>[(0,o.Wm)(m,{caption:""},{default:(0,o.w5)((()=>[(0,o.Uk)("Thêm")])),_:1}),(0,o.Wm)(y,{name:"add_shopping_cart",onClick:a=>t.confirmAddToCart(e.productId)},null,8,["onClick"])])),_:2},1024)])),_:2},1024),(0,o.Wm)(T,{spaced:""})])))),128))])),_:1})])}var i=a(1926),c=a(2274),m=a(5039),p=a(631);const g="siE2Tnd2dwjgMPdkd8331BB7PpCPhvDp",w=(0,i.L)({uri:`https://store.amycos.vn/wordpress/${g}`}),f=new c.h,h=new m.i(((t,e)=>{const a=localStorage.getItem("woo-session");return a&&t.setContext((({headers:t={}})=>({headers:{"woocommerce-session":`Session ${a}`}}))),e(t)})),T=new m.i(((t,e)=>e(t).map((e=>{const a=t.getContext(),{response:{headers:o}}=a,n=o.get("woocommerce-session");return n&&("false"===n?localStorage.removeItem("woo-session"):localStorage.getItem("woo-session")!==n&&localStorage.setItem("woo-session",o.get("woocommerce-session"))),e})))),_=new p.f({link:h.concat(T.concat(w)),cache:f}),y=_;var I=a(7873),W=a(6950),b=a(8359),k=a(4328);const x=(0,o.aZ)({name:"IndexPage",data(){return{products:[],cart:{}}},methods:{confirmAddToCart(t){this.$q.dialog({title:"Thêm vào giỏ hàng",message:"Số lượng",prompt:{model:1,type:"text"},cancel:!0,persistent:!0}).onOk((e=>{this.addToCart({productId:t,quantity:parseInt(e)})}))},async addToCart({productId:t,quantity:e=1}){W.Z.show();try{const{data:a}=await y.mutate({mutation:I.ZP`
+            mutation ($input: AddToCartInput!) {
+              addToCart(input: $input) {
+                cart {
+                  appliedCoupons {
+                    code
+                  }
+                  contents {
+                    nodes {
+                      key
+                      product {
+                        node {
+                          id
+                          databaseId
+                          name
+                          type
+                          featuredImage {
+                            node {
+                              id
+                              sourceUrl
+                            }
+                          }
+                        }
+                      }
+                      variation {
+                        node {
+                          id
+                          databaseId
+                          name
+                          description
+                          type
+                          onSale
+                          price
+                          regularPrice
+                          salePrice
+                          featuredImage {
+                            node {
+                              id
+                              sourceUrl
+                            }
+                          }
+                          attributes {
+                            nodes {
+                              id
+                              name
+                              value
+                            }
+                          }
+                        }
+                      }
+                      quantity
+                      total
+                      subtotal
+                      subtotalTax
+                    }
+                  }
+                  subtotal
+                  subtotalTax
+                  shippingTax
+                  shippingTotal
+                  total
+                  totalTax
+                  feeTax
+                  feeTotal
+                  discountTax
+                  discountTotal
+                }
+              }
+            }
+          `,variables:{input:{productId:parseInt(t),quantity:e}}});W.Z.hide(),this.cart=a.addToCart.cart,k.Z.create({type:"positive",message:"Đã thêm vào giỏ hàng!"})}catch(a){k.Z.create({type:"negative",message:"Không thể thêm sản phẩm hết hàng!"})}},async removeItemsFromCart(t){W.Z.show({spinner:b.Z,spinnerSize:"100px"});try{const{data:e}=await y.mutate({mutation:I.ZP`
+            mutation CLEAR_CART_MUTATION($input: RemoveItemsFromCartInput!) {
+              removeItemsFromCart(input: $input) {
+                cart {
+                  appliedCoupons {
+                    code
+                  }
+                  contents {
+                    nodes {
+                      key
+                      product {
+                        node {
+                          id
+                          databaseId
+                          name
+                          type
+                          featuredImage {
+                            node {
+                              id
+                              sourceUrl
+                            }
+                          }
+                        }
+                      }
+                      variation {
+                        node {
+                          id
+                          databaseId
+                          name
+                          description
+                          type
+                          onSale
+                          price
+                          regularPrice
+                          salePrice
+                          featuredImage {
+                            node {
+                              id
+                              sourceUrl
+                            }
+                          }
+                          attributes {
+                            nodes {
+                              id
+                              name
+                              value
+                            }
+                          }
+                        }
+                      }
+                      quantity
+                      total
+                      subtotal
+                      subtotalTax
+                    }
+                  }
+                  subtotal
+                  subtotalTax
+                  shippingTax
+                  shippingTotal
+                  total
+                  totalTax
+                  feeTax
+                  feeTotal
+                  discountTax
+                  discountTotal
+                }
+              }
+            }
+          `,variables:{input:{keys:t}}});W.Z.hide(),this.cart=e.removeItemsFromCart.cart,k.Z.create({type:"positive",message:"Đã xóa sản phẩm giỏ hàng!"})}catch(e){k.Z.create({type:"negative",message:"Không thể xóa sản phẩm giỏ hàng!"})}},async updateCartItem(t){console.log(t)},async loadProducts(){const{data:t}=await y.query({query:I.ZP`
+          query (
+            $first: Int
+            $after: String
+            $where: RootQueryToProductConnectionWhereArgs
+          ) {
+            products(first: $first, after: $after, where: $where) {
+              nodes {
+                id
+                productId: databaseId
+                averageRating
+                slug
+                sku
+                featuredImage {
+                  node {
+                    id
+                    altText
+                    sourceUrl(size: WOOCOMMERCE_THUMBNAIL)
+                  }
+                }
+                name
+                ... on SimpleProduct {
+                  price
+                  regularPrice
+                  stockQuantity
+                  id
+                }
+                ... on VariableProduct {
+                  price
+                  stockQuantity
+                  id
+                  regularPrice
+                }
+                ... on ExternalProduct {
+                  price
+                  id
+                  externalUrl
+                  regularPrice
+                }
+                ... on GroupProduct {
+                  id
+                  products {
+                    nodes {
+                      ... on SimpleProduct {
+                        id
+                        price
+                        regularPrice
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `});this.products=t.products.nodes},async loadCart(){W.Z.show();const{data:t}=await y.query({query:I.ZP`
+          query GET_CART {
+            cart {
+              contents {
+                nodes {
+                  key
+                  product {
+                    node {
+                      id
+                      databaseId
+                      name
+                      type
+                      featuredImage {
+                        node {
+                          id
+                          sourceUrl
+                        }
+                      }
+                    }
+                  }
+                  variation {
+                    node {
+                      id
+                      databaseId
+                      name
+                      description
+                      type
+                      onSale
+                      price
+                      regularPrice
+                      salePrice
+                      featuredImage {
+                        node {
+                          id
+                          sourceUrl
+                        }
+                      }
+                      attributes {
+                        nodes {
+                          id
+                          name
+                          value
+                        }
+                      }
+                    }
+                  }
+                  quantity
+                  total
+                  subtotal
+                  subtotalTax
+                }
+              }
+              subtotal
+              subtotalTax
+              shippingTax
+              shippingTotal
+              total
+              totalTax
+              feeTax
+              feeTotal
+              discountTax
+              discountTotal
+            }
+          }
+        `});this.cart=t.cart,W.Z.hide()}},mounted(){this.loadCart(),this.loadProducts()}});var v=a(1639),C=a(3246),q=a(3115),U=a(490),P=a(6749),Z=a(990),S=a(7467),$=a(963),Q=a(926),z=a(2857),A=a(9984),H=a.n(A);const R=(0,v.Z)(x,[["render",u]]),D=R;H()(x,"components",{QList:C.Z,QItemLabel:q.Z,QItem:U.Z,QItemSection:P.Z,QBadge:Z.Z,QInput:S.Z,QBtn:$.Z,QSeparator:Q.Z,QIcon:z.Z})}}]);
